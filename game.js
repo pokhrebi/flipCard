74f909e86f215eea15a4ddfc98796f273a21f077
pokhrebi <61396982+pokhrebi@users.noreply.github.com>
2022-08-01 12:46:17 -0400

    creating the functions for the game

---
+++ game.js
@@ -0,0 +1,19 @@
+var game;
+var gameOptions = {
+  flipSpeed: 200,
+  flipZoom: 1.2
+}
+window.onload = function() {
+
+}
+var playGame = function (game){}
+
+playGame.prototype = {
+  preload: function() {
+
+  },
+  create: function() {
+
+  },
+
+}
