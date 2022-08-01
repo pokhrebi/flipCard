d1668595bed8604bd27c7e89aa0f63bca242fb2e
pokhrebi <61396982+pokhrebi@users.noreply.github.com>
2022-08-01 12:47:19 -0400

    start game on load

--- game.js
+++ game.js
@@ -1,10 +1,14 @@
+const { Phaser } = require("./phaser.min");
+
 var game;
 var gameOptions = {
   flipSpeed: 200,
   flipZoom: 1.2
 }
 window.onload = function() {
-
+  game = new Phaser.Game(500, 500);
+  game.state.add("PlayGame", playGame);
+  game.state.start("PlayGame");
 }
 var playGame = function (game){}
 
